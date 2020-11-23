b1c0ef7ca7fdf74712876222798e48d6fe9c1d83
Henrique Costa <henriquemartinsc@gmail.com>
2020-11-22 22:30:20 -0300

    T29

---
+++ pratica/01/ItemNotaFiscal.java
@@ -0,0 +1,13 @@
+public class ItemNotaFiscal {
+    
+    int quantidade;
+    Produto produto = new Produto();
+
+    public float getTotal() {
+
+        float precoComImposto = produto.preco * (1 + produto.alicota);
+        return quantidade * precoComImposto;
+
+    }
+
+}
---
+++ pratica/01/Produto.java
@@ -0,0 +1,7 @@
+public class Produto {
+    
+    String descricao = "descrição do produto";
+    float preco = 2;
+    float alicota = 0.5f;
+
+}
